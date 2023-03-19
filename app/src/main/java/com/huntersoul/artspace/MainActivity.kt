9ac364a81ebd06327c94181c757f5eea1894c7d1
EstebanTM <s18120211@alumnos.itsur.edu.mx>
2023-03-19 10:29:34 -0600

    Agregar imagenes y strings

--- app/src/main/java/com/huntersoul/artspace/MainActivity.kt
+++ app/src/main/java/com/huntersoul/artspace/MainActivity.kt
@@ -3,12 +3,16 @@ package com.huntersoul.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.huntersoul.artspace.ui.theme.ArtSpaceTheme
 
@@ -22,22 +26,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ArtSpace()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ArtSpace(modifier: Modifier = Modifier){
+     Column() {
+         Image(painter = painterResource(id = R.drawable.artpic1), contentDescription = null)
+         Text(text = stringResource(id = R.string.picname1))
+         Text(text = stringResource(id = R.string.autor1))
+     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpace()
     }
 }
