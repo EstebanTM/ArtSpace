f2a03464ea5949005fc0276bdf923cf1771fe5e1
EstebanTM <s18120211@alumnos.itsur.edu.mx>
2023-03-21 11:00:34 -0600

    Agregar botones

--- app/src/main/java/com/huntersoul/artspace/MainActivity.kt
+++ app/src/main/java/com/huntersoul/artspace/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.huntersoul.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,11 +36,32 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun ArtSpace(modifier: Modifier = Modifier){
-     Column() {
-         Image(painter = painterResource(id = R.drawable.artpic1), contentDescription = null)
-         Text(text = stringResource(id = R.string.picname1))
-         Text(text = stringResource(id = R.string.autor1))
-     }
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = painterResource(id = R.drawable.artpic1), contentDescription = null)
+            Text(text = stringResource(id = R.string.picname1))
+            Text(text = stringResource(id = R.string.autor1))
+            Spacer(
+                modifier = Modifier.height((16.dp))
+            )
+            Row() {
+                Button(onClick = { /*TODO*/ }) { Text(stringResource(R.string.preview)) }
+                Button(onClick = { /*TODO*/ }) { Text(stringResource(R.string.next)) }
+            }
+        }
+
+
+
+    //Button(
+      //  onClick = {
+        //    d1 = (1..6).random()
+          //  d2 = (1..6).random()
+        //}
+    //) {
+      //  Text(stringResource(R.string.roll))
+    //}
 }
 
 
--- app/src/main/java/com/huntersoul/artspace/ui/theme/Type.kt
+++ app/src/main/java/com/huntersoul/artspace/ui/theme/Type.kt
@@ -2,17 +2,19 @@ package com.huntersoul.artspace.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.huntersoul.artspace.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
+/*val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    )*/
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
@@ -24,5 +26,31 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     )
-    */
+)
+*/
+
+val Sansita = FontFamily(
+    Font(R.font.sansita_swashed, FontWeight.Normal),
+    Font(R.font.sansita_swashed_semibold, FontWeight.Bold)
+)
+
+val Typography = Typography(
+    defaultFontFamily = Sansita,
+    h1 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    h2 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+
+    body1 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
 )
